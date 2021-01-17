4b5ce1370c16525b44e63b90257604a8d13b0f2e
noa4k <35699438+noa4k@users.noreply.github.com>
2021-01-17 11:24:32 +0530

    my sociohub website

---
+++ about.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>SocioHub</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">SocioHub</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+
+            <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+            <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1>SocioHub</h1>
+  <p>We are a group for the emmerging people and to connect SMB's to help them make their existence in the social market with right categry of people where you can achieve the desired goal of living into the social media with our service. 
+  </p>
+  <div>
+    <h3>Founder</h3>
+    <p> SocioHub was founded by Pulkit Dikshit, Since then we are trying to give best to our customers through our services.
+    </p>
+    <p>We can come up as a good tool for you all. Contact for further queries.</p>
+  </div>
+</div>
+
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6"> 
+      <img src="images/my_pic.jpg" class="image-fluid aboutimg" height="300px">
+    </div>
+    <div class="col-lg-6 col-md-6"> 
+      <h2 class="display-4">"Motive of 'Pulkit' towards 'SocioHub' "</h2>
+      <p class="py-3"> I am Pulkit Dikshit and I am here with all your solutions to boost your social media on any platforms whether it's Facebooko, Instagram, Google Business you name it! Our team is working on to give the best experience possible by us so that you can breathe easily and watch your  social media grow. We provide solutions what you need. This all is possible by My Faculty Mr. Upendera Singh who gave me this opportunity to buid this website as a assignment. </p>
+      <a href="index.php" class="btn btn-outline-success"> Home </a>
+    </div>
+  </div>
+  </div>
+</section>
+
+
+
+<footer>
+  <h3 class="p-3 bg-dark text-white text-center"> @SocioHub Services</h3>
+</footer>
+</body>
+</html>
